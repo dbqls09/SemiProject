a2a878e661e7a6c81da14f1fc94b22ba0452dce2
yasmin <tldkfndl56@naver.com>
2022-10-11 13:47:03 +0900

    221011 two

--- src/main/java/controller/PayController.java
+++ src/main/java/controller/PayController.java
@@ -7,9 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class PayController
- */
 @WebServlet("/pay")
 public class PayController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
